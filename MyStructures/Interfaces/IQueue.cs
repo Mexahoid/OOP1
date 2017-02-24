cd4aae6a296899e701ff444f16f64db29e94b8c0
Mexahoid <mirramax97@Mail.ru>
2017-02-24 14:56:59 +0300

    Переформировка библиотек

---
+++ MyStructures/Interfaces/IQueue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStructs.Interfaces
+{
+    /// <summary>
+    ///  Базовый интерфейс для всех очередей.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IQueue<T> : IStructure<T>
+    {
+        /// <summary>
+        /// Заталкивает элемент в очередь.
+        /// </summary>
+        /// <param name="value">Заталкиваемый элемент.</param>
+        void Enqueue(T value);
+
+        /// <summary>
+        /// Вытаскивает элемент из головы очереди.
+        /// </summary>
+        /// <returns></returns>
+        T Dequeue();
+    }
+}
